96878addc39c11b9de51f1ca058601cd8b36b852
Sergei Sorochinskii <sergei.sorochinskii93@gmail.com>
2025-05-07 14:31:21 +0300

    Using V2 from torchvision

--- lesson_4.py
+++ lesson_4.py
@@ -63,3 +63,75 @@ print(f"{type(img_tensor)=}")
 print(f"{img_tensor.shape=}")  # (C, H, W)
 print(f"{img_tensor.dtype=}")
 print(f"min = {img_tensor.min()}; max = {img_tensor.max()}")
+
+
+# Перетворення зображення в torch.tensor()
+# (PIL.image, np.array) ==> torch.tensor
+# (H, W, C) ==> (C, H, W)
+# (0, 255) ==> (0.0, 1.0)
+transform = v2.ToTensor()
+img_v2_tensor = transform(Image.open("./img/test.jpeg"))
+print(f"{type(img_v2_tensor)=}")
+print(f"{img_v2_tensor.shape=}")  # (C, H, W)
+print(f"{img_v2_tensor.dtype=}")
+print(f"min = {img_v2_tensor.min()}; max = {img_v2_tensor.max()}")
+
+
+# Перетворення зображення в torchvision.tv_tensors.
+# (PIL.image, np.array) ==> torchvision.tv_tensors
+# (H, W, C) ==> (C, H, W)
+# (0, 255) ==> (0.0, 1.0)
+
+transform = v2.ToImage()
+img_dtype_tensor = transform(Image.open("./img/test.jpeg"))
+print(f"{type(img_dtype_tensor)=}")
+print(f"{img_dtype_tensor.shape=}")  # (C, H, W)
+print(f"{img_v2_tensor.dtype=}")
+print(f"min = {img_dtype_tensor.min()}; max = {img_dtype_tensor.max()}")
+
+
+# Послідовне застосування трансформацій
+transform = v2.Compose(
+    transforms=[
+        v2.ToImage(),
+        v2.ToDtype(torch.float32, scale=True),
+        v2.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
+    ]
+)
+img_final_tensor = transform(Image.open("./img/test.jpeg"))
+print(f"{type(img_tensor)=}")
+print(f"{img_final_tensor.shape=}")  # (C, H, W)
+print(f"{img_final_tensor.dtype=}")
+print(f"min = {img_final_tensor.min()}; max = {img_final_tensor.max()}")
+
+"""
+    Послідовність застосування трансформацій:
+    transform = v2.Compose(
+        transforms=[
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale=True),
+            --- Додавання потрібних переьворень: 
+            v2.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
+        ]
+)
+"""
+
+# Застосування трансформації до dataset
+transform = v2.Compose(
+    transforms=[
+        v2.ToImage(),
+        v2.ToDtype(torch.float32, scale=True),
+        v2.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
+    ]
+)
+
+train_data = ImageFolder(root="./mnist/training", transform=transform)
+test_data = ImageFolder(root="./mnist/testing", transform=transform)
+
+img_obj, cls_label = test_data[2]
+print(f"{img_obj=}")
+print(f"{cls_label=}")
+print(f"{type(img_obj)=}")
+print(f"{img_obj.shape=}")  # (C, H, W)
+print(f"{img_obj.dtype=}")
+print(f"min = {img_obj.min()}; max = {img_obj.max()}")
